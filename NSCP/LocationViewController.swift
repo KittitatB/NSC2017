6a0ac92d0eb5bf18c8fc59eff0d7a57d6a596e58
Kittitat <KittitatBoonk@gmail.com>
2018-01-31 02:05:54 +0700

    loading all location

--- NSCP/LocationViewController.swift
+++ NSCP/LocationViewController.swift
@@ -7,33 +7,51 @@
 //
 
 import UIKit
-
+import Firebase
 class LocationViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     @IBOutlet weak var tableview: UITableView!
-    
-    
+    var locations = [String()]
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-    }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return locations.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath as IndexPath) as! LocationTableViewCell
-        
+        cell.locationName.text = locations[indexPath.item]
         return cell
     }
-
+    
+    func loadData(){
+        let postsRef = Database.database().reference().child("posts")
+        postsRef.observeSingleEvent(of: .value, with: { (SnapShot) in
+            if let postsDictionary = SnapShot.value as? [String: AnyObject]{
+                for postsDic in postsDictionary{
+                    let post = Post()
+                    post.setValuesForKeys(postsDic.value as! [String : Any])
+                    if !self.locations.contains(post.location!){
+                        self.locations.append(post.location!)
+                    }
+                }
+                if self.locations.contains(""){
+                    self.locations.removeFirst()
+                }
+                DispatchQueue.main.async {
+                    self.tableview.reloadData()
+                }
+            }
+            print(self.locations)
+        })
+    }
+    
     
 
 }
